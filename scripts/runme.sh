17516172461ab6547add518727848e1c9677777a
Rob Lemley <rob@thunderbird.net>
2022-01-25 00:24:14 -0500

    go more faster

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -26,7 +26,6 @@ export PATH=$PATH:$APPIMAGETOOL_PATH
 TARGET="Thunderbird-${VERSION}.AppImage"
 TARGET_FULL_PATH="$ARTIFACTS_DIR/$TARGET"
 APPDIR_DEST="${WORKSPACE}/AppDir"
-PRODUCT_DEST="${APPDIR_DEST}/thunderbird"
 DESKTOP_FILE="net.thunderbird.Thunderbird.desktop"
 
 mkdir -p "${ARTIFACTS_DIR}"
@@ -37,17 +36,17 @@ CURL="curl --location --retry 10 --retry-delay 10"
 # Download and extract en-US linux64 binary
 $CURL -o "${WORKSPACE}/${PRODUCT}.tar.bz2" \
     "${CANDIDATES_DIR}/${VERSION}-candidates/build${BUILD_NUMBER}/linux-x86_64/en-US/${PRODUCT}-${VERSION}.tar.bz2"
-tar -C "${APPDIR_DEST}" -xf "${WORKSPACE}/${PRODUCT}.tar.bz2"
+tar -C "${APPDIR_DEST}" --strip-components=1 -xf "${WORKSPACE}/${PRODUCT}.tar.bz2"
 
 # Symlink used for launching
-mkdir -p "${APPDIR_DEST}/usr/bin"
-ln -s ../../${PRODUCT}/${PRODUCT} "${APPDIR_DEST}/usr/bin/"
+#mkdir -p "${APPDIR_DEST}/usr/bin"
+#ln -s ../../${PRODUCT}/${PRODUCT} "${APPDIR_DEST}/usr/bin/"
 
-DISTRIBUTION_DIR="${PRODUCT_DEST}/distribution"
+DISTRIBUTION_DIR="${APPDIR_DEST}/distribution"
 mkdir -p "${DISTRIBUTION_DIR}"
 
 cp -v "${SCRIPT_DIRECTORY}/${DESKTOP_FILE}" "${APPDIR_DEST}"
-cp -v "${PRODUCT_DEST}/chrome/icons/default/default256.png" "${APPDIR_DEST}/net.thunderbird.thunderbird.png"
+cp -v "${APPDIR_DEST}/chrome/icons/default/default256.png" "${APPDIR_DEST}/net.thunderbird.thunderbird.png"
 
 # Add a group policy file to disable app updates, as those are handled by snapd
 cp -v "${SCRIPT_DIRECTORY}/policies.json" "${DISTRIBUTION_DIR}"
@@ -61,7 +60,7 @@ locales=$(cat ${WORKSPACE}/l10n_locales)
 
 mkdir -p "${DISTRIBUTION_DIR}/extensions"
 for locale in ${locales}; do
-    $CURL -o "${PRODUCT_DEST}/distribution/extensions/langpack-${locale}@${PRODUCT}.mozilla.org.xpi" \
+    $CURL -o "${APPDIR_DEST}/distribution/extensions/langpack-${locale}@${PRODUCT}.mozilla.org.xpi" \
         "${CANDIDATES_DIR}/${VERSION}-candidates/build${BUILD_NUMBER}/linux-x86_64/xpi/${locale}.xpi"
 done
 
